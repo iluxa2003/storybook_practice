0af03384d5a227d402da2c1517480c7551367658
iluxa2003 <ilya.yakov.com@gmail.com>
2023-02-07 13:53:22 +0200

    added new buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import propTypes from "prop-types";
+import styles from "./Button.module.css";
 const Button = ({
   label,
   onClick,
@@ -7,6 +8,8 @@ const Button = ({
   borderRadius = "14px",
   size = "medium",
   margin = "5px",
+  iconType = "none",
+  icon,
 }) => {
   let padding = "16px";
   if (size === "small") {
@@ -19,19 +22,25 @@ const Button = ({
     padding = "16px 130px";
   }
   const style = {
-    backgroundColor,
-    border: "none",
+    background: backgroundColor,
     borderRadius,
     padding,
     color,
     margin,
-    cursor: "pointer",
-    fontWeight: "600",
-    minWidth: "47px",
   };
   return (
-    <button style={style} onClick={onClick}>
-      {label}
+    <button className={styles.button} style={style} onClick={onClick}>
+      <img
+        className={
+          styles.button_icon +
+          " " +
+          (iconType === "left" ? styles.left_side : "") +
+          (iconType === "center" ? styles.center : "") +
+          (iconType === "none" ? styles.none : "")
+        }
+        src={icon}
+      />
+      <div className={styles.button_text}>{label}</div>
     </button>
   );
 };
@@ -39,6 +48,9 @@ Button.propTypes = {
   label: propTypes.string,
   borderRadius: propTypes.string,
   backgroundColor: propTypes.string,
+  color: propTypes.string,
+  icon: propTypes.object,
+  iconType: propTypes.oneOf(["left", "center", "none"]),
   size: propTypes.oneOf(["small", "medium", "large"]),
 };
 export default Button;
--- src/stories/RegularBotton.stories.js
+++ src/stories/RegularBotton.stories.js
@@ -1,9 +1,10 @@
 import Button from "../components/Button";
-
+import telegram from "../img/Telegram.svg";
 export default {
   title: "Button",
   component: Button,
 };
+
 const Template = (args) => <Button {...args} />;
 export const CustomButton = Template.bind({});
 CustomButton.args = {
@@ -12,14 +13,27 @@ CustomButton.args = {
   borderRadius: "50%",
   backgroundColor: "blue",
 };
-// export const SmallBlueButton = () => (
-//   <Button size="small" label="+" borderRadius="50%" backgroundColor="blue" />
-// );
-// export const LargeBlackButton = () => (
-//   <Button size="large" label="Get Started" />
-// );
+
 export const LargeBlackButton = Template.bind({});
 LargeBlackButton.args = {
   size: "large",
   label: "Get Started",
+};
+
+export const LargeBlueLeftIconButton = Template.bind({});
+LargeBlueLeftIconButton.args = {
+  size: "large",
+  label: "Telegram",
+  icon: telegram,
+  iconType: "left",
+  backgroundColor: "linear-gradient(180deg, #00AEFA 0%, #0983C5 99.08%)",
+};
+
+export const LargeBlueCenterIconButton = Template.bind({});
+LargeBlueCenterIconButton.args = {
+  size: "large",
+  label: "Telegram",
+  icon: telegram,
+  iconType: "center",
+  backgroundColor: "linear-gradient(180deg, #00AEFA 0%, #0983C5 99.08%)",
 };
